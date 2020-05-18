f175eff310dc160d5109485146d7aa932cd05405
Vyacheslav Voronenko <git@voronenko.info>
2020-05-18 22:49:26 +0200

    splitted CV into partials

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import json
+
+
 def define_env(env):
     """
     This is the hook for the functions (new form)
@@ -6,3 +9,6 @@ def define_env(env):
     env.variables["hello"] = "hello"
     # use dot notation for adding
     env.variables["baz"] = "buz"
+
+    with open('cv/resume.json') as json_file:
+        env.variables["resume"] = json.load(json_file)
